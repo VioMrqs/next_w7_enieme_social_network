a207429ea60ef42a6faf8934382e4f8f88d78711
VioMrqs <violette.marquis@gmail.com>
2022-02-16 14:06:48 +0100

    type fix, postlist ok

--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 
 const PostsList = (userToken) => {
   const [postsData, setPostsData] = useState([]);
-  console.log(userToken);
-  console.log(postsData);
 
     useEffect(() => {
       fetch("http://localhost:1337/posts", {
@@ -24,7 +22,7 @@ const PostsList = (userToken) => {
       {postsData.map((post) => (
         <div className="post__item" key={post.id}>
         <h2>{post.text}</h2>
-        <h3>{post.user}</h3>
+        <h3>{post.user.username}</h3>
       </div>
       ))}
     </div>
@@ -32,77 +30,3 @@ const PostsList = (userToken) => {
 };
 
 export default PostsList;
-
-//         // <TodoItem
-//         //   todo={todo}
-//         //   onToggle={onToggle}
-//         //   onDelete={onDelete}
-//         //   key={todo.id}
-//         // />
-
-// import React from "react";
-
-// class PostsList extends React.Component {
-//   // State of your application
-//   state = {
-//     posts: [],
-//     error: null,
-//   };
-
-//   // const [posts, setPosts] = useState([]);
-
-//   // Fetch your restaurants immediately after the component is mounted
-//   componentDidMount = async () => {
-//     // Parses the JSON returned by a network request
-//     const parseJSON = (resp) => (resp.json ? resp.json() : resp);
-
-//     // Checks if a network request came back fine, and throws an error if not
-//     const checkStatus = (resp) => {
-//       if (resp.status >= 200 && resp.status < 300) {
-//         return resp;
-//       }
-//       return parseJSON(resp).then((resp) => {
-//         throw resp;
-//       });
-//     };
-//     const headers = {
-//       "Content-Type": "application/json",
-//     };
-
-//     try {
-//       const posts = await fetch("http://localhost:1337/posts", {
-//         method: "GET",
-//         headers: headers,
-//       })
-//         .then(checkStatus)
-//         .then(parseJSON);
-//       this.setState({ posts });
-//     } catch (error) {
-//       this.setState({ error });
-//     }
-//   };
-
-//   render() {
-//     const { error, posts } = this.state;
-
-//     // Print errors if any
-//     if (error) {
-//       return <div>An error occured: {error.message}</div>;
-//     }
-
-//     return (
-//       <div className="App">
-//         <ul>
-//           {console.log(this.state.posts)}
-//           {this.state.posts.map((restaurant) => (
-//             <li key={restaurant.id}>
-//               {restaurant.text} by {restaurant.user.username}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default PostsList;
